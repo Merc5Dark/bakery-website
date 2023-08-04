5517b21f8ab9d7e0cf9c034c46400909e2f4fc83
Merc5Dark <31131794siya@gmail.com>
2023-08-04 15:08:50 +0200

    css fix

--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import styles from './MenuPage.module.css';
 
 const MenuPage = () => {
+  const menuItems = [
+    {
+      name: 'Pastry 1',
+      description: 'Description for Pastry 1',
+      imageSrc: 'path/to/pastry1.jpg', // Replace with the actual image path
+    },
+    // Add more menu items as needed
+  ];
+
   return (
-    <div>
-      <h1>Menu</h1>
-      <ul>
-        <li>Pastry 1</li>
-        <li>Pastry 2</li>
-        <li>Pastry 3</li>
-        <li>Pastry 4</li>
-        <li>Pastry 5</li>
-      </ul>
-      {/* Add images for each pastry */}
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Menu</h1>
+      <div>
+        {menuItems.map((item, index) => (
+          <div key={index} className={styles.menuItem}>
+            <img src={item.imageSrc} alt={item.name} className={styles.menuItemImage} />
+            <div className={styles.menuItemName}>{item.name}</div>
+            <div className={styles.menuItemDescription}>{item.description}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
