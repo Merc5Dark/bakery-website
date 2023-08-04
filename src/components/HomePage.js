15da94e368379d27aa62d79fc81bbc057c197654
Merc5Dark <31131794siya@gmail.com>
2023-08-04 14:50:46 +0200

    css work

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Our Bakery</h1>
-      <img src="path/to/your/image" alt="Bakery" />
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Welcome to Our Bakery</h1>
+      <img
+        src="path/to/your/image"  // Replace with the actual path to your image
+        alt="Bakery"
+        className={styles.image}
+      />
     </div>
   );
 };
