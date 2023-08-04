1dc2b69ab14c3ab5665020476330ddd557243032
Merc5Dark <31131794siya@gmail.com>
2023-08-04 15:13:00 +0200

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Navigation.module.css';
 
 const Navigation = () => {
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navList}>
-        <li className={styles.navItem}>
-          <Link to="/" className={styles.navLink}>Home</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link to="/about" className={styles.navLink}>About Us</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link to="/menu" className={styles.navLink}>Menu</Link>
-        </li>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
       </ul>
     </nav>
   );
